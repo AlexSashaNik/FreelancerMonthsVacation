1c0af03f67ea11c9f1c35dc2b7c421f822febd00
AlexNikitina <alex.nikitina82@gmail.com>
2023-05-30 10:55:51 +0200

    Test.csv added

--- src/test/java/MonthsNumberCalcTest.java
+++ src/test/java/MonthsNumberCalcTest.java
@@ -1,17 +1,23 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.javaqa.FreelancerMonthsVacation.monthsNumbers.MonthsNumberCalc;
 
 
 public class MonthsNumberCalcTest {
 
-    @Test
-    void numberOfMonthsToBeOffCases() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/Test.csv")
+    void numberOfMonthsToBeOffCases(int income, int expenses, int threshold, int expected) {
         MonthsNumberCalc calc = new MonthsNumberCalc();
 
-        int expected = 3;
-        int actual = calc.calculation(10000, 3000, 20000);
+        //int expected = 3;
+        //Расчет количества свободных месяцев
+        int actual = calc.calculation(income, expenses, threshold);
 
+        //Использование метода Assertions для сравнения ожидаемого результата и фактического
         Assertions.assertEquals(expected, actual);
 
     }
